f32698ea25d2ca5890fa810bbb1da13959a45d74
Yuri <yurifl03@gmail.com>
2024-05-29 19:09:29 -0300

    wip

--- nix/colmena/flake.nix
+++ nix/colmena/flake.nix
@@ -1,35 +1,42 @@
-# nix build .#nixosConfigurations.rpi.config.system.build.sdImage
-# nix flake check
 {
   description = "NixOS configuration for Raspberry Pi";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }: 
-    let
-      system = "aarch64-linux";
-    in
+
     {
-      nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-          # ({ config, lib, pkgs, ... }: {
-          #   imports = [ ./sd-image.nix ];
-          # })
-          ../sd-image.nix
-          ../hardware-configuration.nix
-          ../tailscale.nix
-          { nixpkgs.config.warn-dirty = false; }
-        ];
-      };
+      # nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
+      #   inherit system;
+      #   modules = [
+      #     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+      #     "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+      #     # ({ config, lib, pkgs, ... }: {
+      #     #   imports = [ ./sd-image.nix ];
+      #     # })
+      #     ../sd-image.nix
+      #     ../hardware-configuration.nix
+      #     ../tailscale.nix
+      #     { nixpkgs.config.warn-dirty = false; }
+      #   ];
+      # };
       colmena = {
         meta = {
-          nixpkgs = import nixpkgs {
-            system = "aarch64-linux";
-          };
+          # nixpkgs = nixpkgs.legacyPackages.aarch64-linux;
         };
+        # defaults = { pkgs, lib, name, nodes, meta, ... }: {
+        #   imports = [
+        #     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+        #     "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+        #     # ({ config, lib, pkgs, ... }: {
+        #     #   imports = [ ./sd-image.nix ];
+        #     # })
+        #     ../sd-image.nix
+        #     ../hardware-configuration.nix
+        #     ../tailscale.nix
+        #     { nixpkgs.config.warn-dirty = false; }
+        #   ];
+        # };
 
         host-b = {
           deployment = {
