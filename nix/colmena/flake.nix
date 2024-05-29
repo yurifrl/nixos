4fb2a6427f62d665dcf49d0870c9b4e5d716beca
Yuri <yurifl03@gmail.com>
2024-05-29 19:11:25 -0300

    wip

--- nix/colmena/flake.nix
+++ nix/colmena/flake.nix
@@ -4,25 +4,29 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }: 
-
+    let
+      system = "aarch64-linux";
+    in
     {
-      # nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
-      #   inherit system;
-      #   modules = [
-      #     "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-      #     "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-      #     # ({ config, lib, pkgs, ... }: {
-      #     #   imports = [ ./sd-image.nix ];
-      #     # })
-      #     ../sd-image.nix
-      #     ../hardware-configuration.nix
-      #     ../tailscale.nix
-      #     { nixpkgs.config.warn-dirty = false; }
-      #   ];
-      # };
+      nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
+          # ({ config, lib, pkgs, ... }: {
+          #   imports = [ ./sd-image.nix ];
+          # })
+          ../sd-image.nix
+          ../hardware-configuration.nix
+          ../tailscale.nix
+          { nixpkgs.config.warn-dirty = false; }
+        ];
+      };
       colmena = {
         meta = {
-          # nixpkgs = nixpkgs.legacyPackages.aarch64-linux;
+          nixpkgs = import nixpkgs {
+            inherit system; # Change here to use the system variable
+          };
         };
         # defaults = { pkgs, lib, name, nodes, meta, ... }: {
         #   imports = [
