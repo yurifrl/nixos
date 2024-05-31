5857bf9ccabec169152f6e07c29fc076af898a87
Yuri <yurifl03@gmail.com>
2024-05-30 21:20:02 -0300

    wip

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,58 +1,62 @@
 {
-  description = "NixOS configuration for Raspberry Pi";
+  description = "NixOS configuration for Raspberry Pi and VirtualBox VM on Intel Mac";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: 
-  let
-    system = "aarch64-linux";
-    protoName = "proto";
-  in
-  {
+  outputs = { self, nixpkgs, ... }: {
     nixosConfigurations.rpi = nixpkgs.lib.nixosSystem {
-      inherit system;
+      system = "aarch64-linux";
       modules = [
         "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
         "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-        { nixpkgs.config.warn-dirty = true; }
         ./common.nix
         ./tailscale.nix
-        # ./machines/${protoName}/definition.nix
+        ./machines/rpi/definition.nix
       ];
     };
 
-    defaultPackage.${system} = self.nixosConfigurations.rpi.config.system.build.sdImage;
-    defaultPackage.x86_64-linux = self.nixosConfigurations.rpi.config.system.build.sdImage;
+    nixosConfigurations.vm = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+        ./common.nix
+        ./tailscale.nix
+        ./machines/vm/definition.nix
+      ];
+    };
 
-    # colmena = {
-    #   meta = {
-    #     nixpkgs = import nixpkgs {
-    #       system = "aarch64-linux";
-    #       overlays = [];
-    #     };
-    #   };
+    defaultPackage.aarch64-linux = self.nixosConfigurations.rpi.config.system.build.sdImage;
+    defaultPackage.x86_64-linux = self.nixosConfigurations.vm.config.system.build.isoImage;
 
-    #   defaults = { pkgs, lib, name, nodes, meta, ... }: {
-    #     imports = [
-    #       ./machines/${name}/definition.nix
-    #       ./common.nix
-    #       ./tailscale.nix
-    #     ];
+    colmena = {
+      meta = {
+        nixpkgs = import nixpkgs {
+          system = "aarch64-linux";
+          overlays = [];
+        };
+      };
 
-    #     deployment = {
-    #       buildOnTarget = lib.mkDefault true;
-    #     };
-    #   };
+      defaults = { pkgs, lib, name, nodes, meta, ... }: {
+        imports = [
+          ./machines/${name}/definition.nix
+          ./common.nix
+          ./tailscale.nix
+        ];
+
+        deployment = {
+          buildOnTarget = lib.mkDefault true;
+        };
+      };
       
-    #   proto = {
-    #     deployment = {
-    #       targetHost = "192.168.68.108";
-    #       targetUser = "nixos";
-    #     };
-    #     boot.isContainer = true;
-    #     time.timeZone = "America/Los_Angeles";
-    #   };
-    # };
+      rpi = {
+        deployment = {
+          targetHost = "192.168.68.108";
+          targetUser = "nixos";
+        };
+        boot.isContainer = true;
+        time.timeZone = "America/Los_Angeles";
+      };
+    };
   };
 }
