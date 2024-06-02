6dc15db78896ae4b22b53b7d6e90e76d72fecb3f
Yuri <yurifl03@gmail.com>
2024-06-02 18:43:21 -0300

    wip

--- nix/packages/cowsay-version.nix
+++ nix/packages/cowsay-version.nix
@@ -1,10 +1,13 @@
 # nix-build -E '(import <nixpkgs> {}).callPackage ./cowsay-version.nix {}'  
 { cowsay, stdenv }:
 
+let
+  version = "0.0.9";
+in
 stdenv.mkDerivation {
   name = "cowsay-version";
-  src = ./.;  # This sets the source directory to the current directory
-
+  src = ../../.;
+  
   phases = [ "installPhase" ];
 
   propagatedBuildInputs = [ cowsay ];
@@ -13,8 +16,9 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     echo '#!/bin/sh' > $out/bin/cowsay-version
-    
-    echo '${cowsay}/bin/cowsay "Home Automation Systems Version $(cat $src/VERSION)"' >> $out/bin/cowsay-version
+    # TODO: make this work
+    # echo '${cowsay}/bin/cowsay "Home Automation Systems Version $(cat $src/VERSION)"' >> $out/bin/cowsay-version
+    echo '${cowsay}/bin/cowsay "Home Automation Systems Version ${version}"' >> $out/bin/cowsay-version
     chmod +x $out/bin/cowsay-version
   '';
 }
