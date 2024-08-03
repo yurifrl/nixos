35b3a23266051711b3609a6582c7a4221b88041d
Yuri <yurifl03@gmail.com>
2024-08-03 06:40:41 -0300

    feat(flake.nix): add deploy-rs as an input to the flake to enable deployment of nodes using deploy-rs library feat(flake.nix): introduce deploy.nodes.some-random-system configuration for deploying a system with specific hostname and user feat(flake.nix): add deploy-rs deployChecks to perform checks and prevent possible mistakes in deployment configurations

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,9 +2,10 @@
   description = "NixOS configuration for Raspberry Pi and VirtualBox VM on Intel Mac";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, deploy-rs, ... }: {
     nixosConfigurations = {
       rpi = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -36,60 +37,17 @@
       x86_64-linux.default = self.nixosConfigurations.vm.config.system.build.isoImage;
     };
 
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
-          overlays = [ ];
-        };
-      };
-
-
-      defaults = { lib, name, ... }: {
-        imports = [
-          ./common.nix
-          ./modules
-          ./machines/${name}/definition.nix
-        ];
-
-        networking.hostName = lib.mkDefault name;
-
-        deployment = {
-          buildOnTarget = lib.mkDefault true;
-
-          keys = {
-            "tailscale-token" = {
-              keyFile = "/src/secrets/${name}/tailscale-token";
-              user = "tailscale";
-              group = "tailscale";
-              permissions = "0400";
-            };
-          };
-        };
-      };
-
-      rpi = {
-        imports = [
-          "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix"
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
-        ];
-        deployment = {
-          targetHost = "192.168.68.102";
-          targetUser = "nixos";
-        };
-      };
-
-      vm = {
-        imports = [
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-        ];
-        # Via tailscale
-        deployment = {
-          targetHost = "127.0.0.1";
-          targetPort = 2222;
-          targetUser = "nixos";
+    deploy.nodes.some-random-system = {
+      hostname = "192.168.68.102";
+      profiles = {
+        system = {
+          user = "nixos";
+          path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.rpi;
         };
       };
     };
+
+    # This is highly advised, and will prevent many possible mistakes
+    checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
   };
 }
