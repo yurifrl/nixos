3db412fcd867ba2120a4b376722486cf468293c6
Yuri <yurifl03@gmail.com>
2024-08-09 19:38:08 -0300

    wip

--- nix/machines/rpi/kubernetes.nix
+++ nix/machines/rpi/kubernetes.nix
@@ -8,29 +8,28 @@ let
 in
 {
   # resolve master hostname
-  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+  # networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
 
   # packages for administration tasks
   environment.systemPackages = with pkgs; [
-    kompose
     kubectl
     kubernetes
   ];
 
   services.kubernetes = {
     roles = ["master" "node"];
-    masterAddress = kubeMasterHostname;
-    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    easyCerts = true;
-    apiserver = {
-      securePort = kubeMasterAPIServerPort;
-      advertiseAddress = kubeMasterIP;
-    };
+    # masterAddress = kubeMasterHostname;
+    # apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+    # easyCerts = true;
+    # apiserver = {
+    #   securePort = kubeMasterAPIServerPort;
+    #   advertiseAddress = kubeMasterIP;
+    # };
 
-    # use coredns
-    addons.dns.enable = true;
+    # # use coredns
+    # addons.dns.enable = true;
 
-    # needed if you use swap
-    kubelet.extraOpts = "--fail-swap-on=false";
+    # # needed if you use swap
+    # kubelet.extraOpts = "--fail-swap-on=false";
   };
 }
