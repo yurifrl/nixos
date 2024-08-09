72aa2c739ecf32cde6130305602846a626c827c5
Yuri <yurifl03@gmail.com>
2024-08-09 20:04:14 -0300

    wip

--- nix/machines/rpi/kubernetes.nix
+++ nix/machines/rpi/kubernetes.nix
@@ -1,17 +1,18 @@
-{ pkgs, ... }:
+{  pkgs, ... }:
 let
   # When using easyCerts=true the IP Address must resolve to the master on creation.
-  # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+ # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
   kubeMasterIP = "10.1.1.2";
   kubeMasterHostname = "api.kube";
   kubeMasterAPIServerPort = 6443;
 in
 {
   # resolve master hostname
-  # networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
+  networking.extraHosts = "${kubeMasterIP} ${kubeMasterHostname}";
 
   # packages for administration tasks
   environment.systemPackages = with pkgs; [
+    kompose
     kubectl
     kubernetes
   ];
@@ -19,17 +20,17 @@ in
   services.kubernetes = {
     roles = ["master" "node"];
     masterAddress = kubeMasterHostname;
-    # apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
-    # easyCerts = true;
-    # apiserver = {
-    #   securePort = kubeMasterAPIServerPort;
-    #   advertiseAddress = kubeMasterIP;
-    # };
+    apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
+    easyCerts = true;
+    apiserver = {
+      securePort = kubeMasterAPIServerPort;
+      advertiseAddress = kubeMasterIP;
+    };
 
-    # # use coredns
-    # addons.dns.enable = true;
+    # use coredns
+    addons.dns.enable = true;
 
-    # # needed if you use swap
-    # kubelet.extraOpts = "--fail-swap-on=false";
+    # needed if you use swap
+    kubelet.extraOpts = "--fail-swap-on=false";
   };
 }
