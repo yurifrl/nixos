b22ee22b163e8e3e8ef5578b77e7d8fe591095d4
Yuri <yurifl03@gmail.com>
2024-08-09 21:43:37 -0300

    wip

--- nix/machines/rpi/kubernetes.nix
+++ nix/machines/rpi/kubernetes.nix
@@ -1,10 +1,9 @@
-{  pkgs, ... }:
+{ pkgs, ... }:
 let
   # When using easyCerts=true the IP Address must resolve to the master on creation.
-  # So use simply 127.0.0.1 in that case.
-  # Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+ # So use simply 127.0.0.1 in that case. Otherwise you will have errors like this https://github.com/NixOS/nixpkgs/issues/59364
+  kubeMasterIP = "127.0.0.1 ";
   # kubeMasterIP = "10.1.1.2";
-  kubeMasterIP = "127.0.0.1";
   kubeMasterHostname = "api.kube";
   kubeMasterAPIServerPort = 6443;
 in
@@ -21,9 +20,7 @@ in
 
   services.kubernetes = {
     roles = ["master" "node"];
-    masterAddress = "localhost";
-
-    # masterAddress = kubeMasterHostname;
+    masterAddress = kubeMasterHostname;
     apiserverAddress = "https://${kubeMasterHostname}:${toString kubeMasterAPIServerPort}";
     easyCerts = true;
     apiserver = {
