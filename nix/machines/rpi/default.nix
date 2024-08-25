4948d482c2966b027a7705d545d0c0a1c091bbe8
Yuri <yurifl03@gmail.com>
2024-08-24 21:08:54 -0300

    :rocket:

--- nix/machines/rpi/default.nix
+++ nix/machines/rpi/default.nix
@@ -10,14 +10,7 @@
     ];
     firewall.enable = false;
     interfaces.eth0.useDHCP = true;
-    # interfaces.eth0 = {
-    #   ipv4.addresses = [
-    #     {
-    #       address = "192.168.68.105";
-    #       prefixLength = 24;
-    #     }
-    #   ];
-    # };
+
     defaultGateway = {
       address = "192.168.68.107";
       interface = "eth0";
--- nix/machines/rpi/kubernetes.nix
+++ nix/machines/rpi/kubernetes.nix
@@ -1,16 +1,57 @@
+{ pkgs, ... }:
+let
+  hosts = [
+    "sergio.localdomain"
+    "nucles.localdomain"
+  ];
+in
 {
-  # Enable Docker
-  virtualisation.docker.enable = true;
-
-  # Install kubeadm, kubectl, and kubelet
-  environment.systemPackages = with pkgs; [
-    kubernetes
-    kubernetes-helm
+  imports = [
+    ../../packages/kube
   ];
 
-  # Enable kubelet service
-  services.kubernetes.roles = [
-    "master"
-    "node"
-  ];
+  services.kubeadm = {
+    enable = true;
+    package = pkgs.kubernetes;
+    kubelet.enable = true;
+
+    init.initConfig = { };
+
+    init.clusterConfig = {
+      clusterName = "nucles";
+
+      controlPlaneEndpoint = "nucles.localdomain:6443";
+
+      apiServer.certSANs = hosts;
+
+      proxy.disabled = true;
+
+      controllerManager.extraArgs = [
+        {
+          name = "bind-address";
+          value = "0.0.0.0";
+        }
+      ];
+
+      scheduler.extraArgs = [
+        {
+          name = "bind-address";
+          value = "0.0.0.0";
+        }
+      ];
+    };
+
+    init.kubeletConfig = {
+      serializeImagePulls = false;
+      allowedUnsafeSysctls = [ "net.ipv4.conf.all.src_valid_mark" ];
+      shutdownGracePeriod = "5m";
+      shutdownGracePeriodCriticalPods = "1m";
+      cpuCFSQuota = false;
+    };
+
+    upgrade.enable = true;
+    upgrade.upgradeConfig = { };
+  };
+
+  environment.systemPackages = [ pkgs.kubernetes ];
 }
