f8f0bada53df71785b0cf77ad9b0a1dfb87023be
Yuri <yurifl03@gmail.com>
2024-11-28 15:57:53 -0300

    wip :zap:

--- nixos/common.nix
+++ nixos/common.nix
@@ -36,7 +36,7 @@ in
     shellAliases = {
       k = "kubectl";
       snixos = "cd /home/nixos/home-systems/nixos && git pull origin main && sudo nixos-rebuild switch --flake .#rpi --impure --show-trace";
-      argo-restart = "cd /home/nixos/home-systems/nixos && git pull origin main & sudo systemctl restart argo-setup & sudo journalctl -u argo-setup.service -f"
+      argo-restart = "cd /home/nixos/home-systems/nixos && git pull origin main & sudo systemctl restart argo-setup & sudo journalctl -u argo-setup.service -f";
     };
   };
 
