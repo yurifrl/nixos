5833504fe6e6d588bc509723a46423b81dc822e0
Yuri <yurifl03@gmail.com>
2024-11-29 19:45:22 -0300

    wip :zap:

--- nixos/modules/kubernetes.nix
+++ nixos/modules/kubernetes.nix
@@ -1,17 +1,18 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Enable required kernel modules for Ceph RBD
-  boot.kernelModules = [ "rbd" ];
-  
-  # Configure kernel parameters for RBD features
-  boot.kernel.sysctl = {
+    # TODO, test rebuilding and flashing ssd with it
+    # Enable required kernel modules for Ceph RBD
+    boot.kernelModules = [ "rbd" ];
+
+    # Configure kernel parameters for RBD features
+    boot.kernel.sysctl = {
     "net.bridge.bridge-nf-call-iptables" = 1;
     "net.bridge.bridge-nf-call-ip6tables" = 1;
-  };
+    };
 
-  # Install required packages
-  environment.systemPackages = with pkgs; [
+    # Install required packages
+    environment.systemPackages = with pkgs; [
     lvm2  # Required for Rook Ceph
-  ];
+    ];
 }
