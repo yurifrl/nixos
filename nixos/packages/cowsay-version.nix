6758acec3f261cfe1af7249deb88b22f1aaa6d73
Yuri <yurifl03@gmail.com>
2024-11-30 11:08:42 -0300

    wip :zap:

--- nixos/packages/cowsay-version.nix
+++ nixos/packages/cowsay-version.nix
@@ -1,26 +1,13 @@
-# nix-build -E '(import <nixpkgs> {}).callPackage ./cowsay-version.nix {}'  
-{ cowsay, stdenv }:
+{ writeShellScriptBin, cowsay }:
 
 let
   version = "0.0.32";
-in
-stdenv.mkDerivation {
-  name = "cowsay-version";
-  src = ../../.;
-
-  phases = [ "installPhase" ];
-
-  propagatedBuildInputs = [ cowsay ];
-  buildInputs = [ cowsay ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    echo '#!/bin/sh' > $out/bin/cv
-    # TODO: make this work
-    # echo '${cowsay}/bin/cowsay "Home Automation Systems Version $(cat $src/VERSION)"' >> $out/bin/cv
-    echo '${cowsay}/bin/cowsay "Home Automation Systems Version ${version}"' >> $out/bin/cv
-    chmod +x $out/bin/cv
+  
+  script = ''
+    #!/bin/sh
+    ${cowsay}/bin/cowsay "Home Automation Systems Version ${version}"
   '';
-}
+in
+writeShellScriptBin "cv" script
 
 
