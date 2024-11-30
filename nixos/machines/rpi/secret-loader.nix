212f461929b19d4be745f0c80800ff4dd6f5ed36
Yuri <yurifl03@gmail.com>
2024-11-30 12:12:25 -0300

    wip :zap:

--- nixos/machines/rpi/secret-loader.nix
+++ nixos/machines/rpi/secret-loader.nix
@@ -5,39 +5,29 @@ let
   secretScriptPath = "/etc/secrets/sync.sh";
 in
 {
-  options.secret-loader.enable = lib.mkOption {
-    type = lib.types.bool;
-    default = false;
-    description = "Enable the secret loader service to execute a script.";
-  };
+  systemd.services.secret-loader = {
+    description = "Secret Loader Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
 
-  config = lib.mkIf config.secret-loader.enable {
-    systemd.services.secret-loader = {
-      description = "Secret Loader Service";
-      after = [ "network.target" ];
-      before = [ "argo-setup.service" ];
-      wantedBy = [ "multi-user.target" ];
-      requiredBy = [ "argo-setup.service" ];
-
-      serviceConfig = {
-        ExecStart = "${secretScriptPath}";
-        # Ensure the script is executable
-        ExecStartPre = "${pkgs.coreutils}/bin/chmod +x ${secretScriptPath}";
-        # Run as a specific user, e.g., 'root' or another user
-        User = "root";
-        # Optionally, specify a working directory
-        WorkingDirectory = "/etc/secrets";
-        
-        # Add retry logic
-        Restart = "always";
-        RestartSec = "30s";
-        StartLimitIntervalSec = "0"; # Disable the start limit interval
-        StartLimitBurst = "0";       # Disable the start limit burst
-      };
-
-      # Restart the service on failure
-      restartIfChanged = true;
-      restartTriggers = [ secretScriptPath ];
+    serviceConfig = {
+      ExecStart = "${secretScriptPath}";
+      # Ensure the script is executable
+      ExecStartPre = "${pkgs.coreutils}/bin/chmod +x ${secretScriptPath}";
+      # Run as a specific user, e.g., 'root' or another user
+      User = "root";
+      # Optionally, specify a working directory
+      WorkingDirectory = "/etc/secrets";
+      
+      # Add retry logic
+      Restart = "always";
+      RestartSec = "30s";
+      StartLimitIntervalSec = "0"; # Disable the start limit interval
+      StartLimitBurst = "0";       # Disable the start limit burst
     };
+
+    # Restart the service on failure
+    restartIfChanged = true;
+    restartTriggers = [ secretScriptPath ];
   };
-}
+} 
