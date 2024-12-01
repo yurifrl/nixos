dc3f57a6efd7727bc1c4769144174ddc889a6940
Yuri <yurifl03@gmail.com>
2024-12-01 18:52:19 -0300

    wip :zap:

--- k8s/charts/rook-ceph-support/start-again.sh
+++ k8s/charts/rook-ceph-support/start-again.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-echo "Attempting normal deletion..."
-kubectl -n rook-ceph delete cephcluster my-cluster
-kubectl -n rook-ceph delete cephblockpool builtin-mgr
+kubectl -n rook-ceph annotate cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup rook.io/force-deletion="true"
+kubectl -n rook-ceph delete cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup
 
-echo "Attempting force deletion..."
-kubectl -n rook-ceph delete cephcluster my-cluster --grace-period=0 --force
-kubectl -n rook-ceph delete cephblockpool builtin-mgr --grace-period=0 --force
+# echo "Attempting normal deletion..."
+# kubectl -n rook-ceph delete cephcluster my-cluster
+# kubectl -n rook-ceph delete cephblockpool builtin-mgr
 
-echo "Removing finalizers..."
-kubectl -n rook-ceph patch cephcluster my-cluster --type merge -p '{"metadata":{"finalizers": []}}'
-kubectl -n rook-ceph patch cephblockpool builtin-mgr --type merge -p '{"metadata":{"finalizers": []}}'
+# echo "Attempting force deletion..."
+# kubectl -n rook-ceph delete cephcluster my-cluster --grace-period=0 --force
+# kubectl -n rook-ceph delete cephblockpool builtin-mgr --grace-period=0 --force
 
-# echo "Last resort: "
-# kubectl proxy --port=8080 &
-# PROXY_PID=$!
-# curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephclusters/my-cluster
-# curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephblockpools/builtin-mgr
-# kill $PROXY_PID
+# echo "Removing finalizers..."
+# kubectl -n rook-ceph patch cephcluster my-cluster --type merge -p '{"metadata":{"finalizers": []}}'
+# kubectl -n rook-ceph patch cephblockpool builtin-mgr --type merge -p '{"metadata":{"finalizers": []}}'
+
+# # echo "Last resort: "
+# # kubectl proxy --port=8080 &
+# # PROXY_PID=$!
+# # curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephclusters/my-cluster
+# # curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephblockpools/builtin-mgr
+# # kill $PROXY_PID
 
 
 
