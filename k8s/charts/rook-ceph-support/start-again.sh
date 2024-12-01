577438960af25f864d0c8eed387fe1a2e1bbb32a
Yuri <yurifl03@gmail.com>
2024-12-01 18:58:53 -0300

    wip :zap:

--- k8s/charts/rook-ceph-support/start-again.sh
+++ k8s/charts/rook-ceph-support/start-again.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-kubectl -n rook-ceph annotate cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup rook.io/force-deletion="true"
-kubectl -n rook-ceph delete cephfilesystemsubvolumegroups.ceph.rook.io my-subvolumegroup
+#!/bin/bash
 
-# echo "Attempting normal deletion..."
-# kubectl -n rook-ceph delete cephcluster my-cluster
-# kubectl -n rook-ceph delete cephblockpool builtin-mgr
+# Annotate resources for force deletion
+kubectl -n rook-ceph annotate cephcluster my-cluster rook.io/force-delete-cluster-on-removal=true
+kubectl -n rook-ceph annotate cephblockpool builtin-mgr rook.io/force-delete-pool-on-removal=true
 
-# echo "Attempting force deletion..."
-# kubectl -n rook-ceph delete cephcluster my-cluster --grace-period=0 --force
-# kubectl -n rook-ceph delete cephblockpool builtin-mgr --grace-period=0 --force
-
-# echo "Removing finalizers..."
-# kubectl -n rook-ceph patch cephcluster my-cluster --type merge -p '{"metadata":{"finalizers": []}}'
-# kubectl -n rook-ceph patch cephblockpool builtin-mgr --type merge -p '{"metadata":{"finalizers": []}}'
-
-# # echo "Last resort: "
-# # kubectl proxy --port=8080 &
-# # PROXY_PID=$!
-# # curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephclusters/my-cluster
-# # curl -k -X DELETE http://127.0.0.1:8080/apis/ceph.rook.io/v1/namespaces/rook-ceph/cephblockpools/builtin-mgr
-# # kill $PROXY_PID
+# Delete the resources
+kubectl -n rook-ceph delete cephcluster my-cluster
+kubectl -n rook-ceph delete cephblockpool builtin-mgr
 
 
+echo "Removing finalizers..."
+kubectl -n rook-ceph patch cephcluster my-cluster --type merge -p '{"metadata":{"finalizers": []}}'
+kubectl -n rook-ceph patch cephblockpool builtin-mgr --type merge -p '{"metadata":{"finalizers": []}}'
 
+
+# Check the resources
 kubectl -n rook-ceph get cephcluster,cephblockpool
