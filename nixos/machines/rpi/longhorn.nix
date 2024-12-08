fecb630c204adfc350b76a51c13d89227becd7df
Yuri <yurifl03@gmail.com>
2024-12-08 19:02:56 -0300

    wip :zap:

--- nixos/machines/rpi/longhorn.nix
+++ nixos/machines/rpi/longhorn.nix
@@ -6,4 +6,32 @@
     # https://github.com/longhorn/longhorn/issues/2166
     # ============================================================================
     
+    # Required packages for Longhorn
+    environment.systemPackages = with pkgs; [
+        open-iscsi
+        nfs-utils
+        cryptsetup
+    ];
+
+    # Enable required kernel modules
+    boot.kernelModules = [
+        "dm_crypt"    # For encryption support
+        "iscsi_tcp"   # For iSCSI support
+    ];
+
+    # Enable required services
+    services = {
+        iscsi = {
+            enable = true;
+            name = "iqn.2024-01.org.nixos:${config.networking.hostName}";
+        };
+        nfs.server.enable = true;
+    };
+
+    # Ensure mount points exist
+    systemd.tmpfiles.rules = [
+        "d /var/lib/longhorn 0700 root root -"
+        "d /host/etc 0755 root root -"
+        "L+ /host/etc/os-release - - - - /etc/os-release"
+    ];
 }
