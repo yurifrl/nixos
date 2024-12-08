041683e7c26635701a89e5a6df0cf613552ec2ab
Yuri <yurifl03@gmail.com>
2024-12-08 19:21:26 -0300

    wip :zap:

--- nixos/machines/rpi/longhorn.nix
+++ nixos/machines/rpi/longhorn.nix
@@ -6,25 +6,52 @@
     # https://github.com/longhorn/longhorn/issues/2166
     # ============================================================================
     
-    # Add symlink for standard paths that Longhorn expects
+    # Ensure required system utilities are available
+    environment.systemPackages = with pkgs; [
+        iscsi-initiator-utils
+        nfs-utils
+        curl
+        findmnt
+        util-linux
+        grep
+        blkid
+    ];
+
+    # Enable required services
+    services.openiscsi = {
+        enable = true;
+        name = "iqn.2024-01.org.nixos.initiator:${config.networking.hostName}";
+    };
+    services.nfs.server.enable = true;
+
+    # Create symlink for standard paths that Longhorn expects
     systemd.tmpfiles.rules = [
         "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
     ];
 
-    # Enable and configure iSCSI service
-    services.openiscsi = {
-        enable = true;
-        name = "iqn.2024-01.org.nixos:${config.networking.hostName}";
-    };
-
     # Configure Docker to use json-file logging instead of journald
-    virtualisation.docker = {
-        enable = true;
-        logDriver = "json-file";
+    virtualisation.docker.logDriver = "json-file";
+
+    # Create os-release file that Longhorn checks for
+    environment.etc."os-release".text = ''
+        NAME="NixOS"
+        ID=nixos
+        VERSION="${config.system.stateVersion}"
+        VERSION_ID="${config.system.stateVersion}"
+        PRETTY_NAME="NixOS ${config.system.stateVersion}"
+        HOME_URL="https://nixos.org/"
+    '';
+
+    # Mount points and filesystem requirements
+    fileSystems."/var/lib/longhorn" = {
+        device = "tmpfs";
+        fsType = "tmpfs";
+        options = [ "size=1G" ];
     };
 
-    # Ensure required packages are available
-    environment.systemPackages = with pkgs; [
-        openiscsi
+    # Required kernel modules
+    boot.kernelModules = [
+        "iscsi_tcp"
+        "nfs"
     ];
 }
