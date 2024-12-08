5870259ec1cd2daa0728d79e993eca4aaed0d2e1
yurifrl <yurilf03@gmail.com>
2024-12-08 14:46:00 -0800

    wip :zap:

--- nixos/machines/rpi/longhorn.nix
+++ nixos/machines/rpi/longhorn.nix
@@ -6,51 +6,20 @@
     # https://github.com/longhorn/longhorn/issues/2166
     # ============================================================================
     
-    # Ensure required system utilities are available
-    environment.systemPackages = with pkgs; [
-        openiscsi
-        nfs-utils
-        curl
-        util-linux
-        grep
-        blkid
-    ];
-
-    # Enable required services
-    services.openiscsi = {
-        enable = true;
-        name = "iqn.2024-01.org.nixos.initiator:${config.networking.hostName}";
-    };
-    services.nfs.server.enable = true;
-
-    # Create symlink for standard paths that Longhorn expects
+    # Add symlink for standard paths that Longhorn expects
     systemd.tmpfiles.rules = [
         "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
     ];
 
-    # Configure Docker to use json-file logging instead of journald
-    virtualisation.docker.logDriver = "json-file";
-
-    # Create os-release file that Longhorn checks for
-    environment.etc."os-release".text = ''
-        NAME="NixOS"
-        ID=nixos
-        VERSION="${config.system.stateVersion}"
-        VERSION_ID="${config.system.stateVersion}"
-        PRETTY_NAME="NixOS ${config.system.stateVersion}"
-        HOME_URL="https://nixos.org/"
-    '';
-
-    # Mount points and filesystem requirements
-    fileSystems."/var/lib/longhorn" = {
-        device = "tmpfs";
-        fsType = "tmpfs";
-        options = [ "size=1G" ];
+    # Enable and configure iSCSI service
+    services.openiscsi = {
+        enable = true;
+        name = "iqn.2024-01.org.nixos:${config.networking.hostName}";
     };
 
-    # Required kernel modules
-    boot.kernelModules = [
-        "iscsi_tcp"
-        "nfs"
-    ];
+    # Configure Docker to use json-file logging instead of journald
+    virtualisation.docker = {
+        enable = true;
+        logDriver = "json-file";
+    };
 }
