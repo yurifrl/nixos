3eae2dde78c4f988f22c6044b26185838a4d9caa
Yuri <yurifl03@gmail.com>
2024-12-09 22:50:04 -0300

    wip :zap:

--- nixos/machines/rpi/longhorn.nix
+++ nixos/machines/rpi/longhorn.nix
@@ -17,12 +17,6 @@
         "dm_mirror"
         "dm_thin_pool"
         "dm_crypt"
-        # Add NFS modules explicitly
-        "nfs"
-        "nfsv4"
-        "nfs_acl"
-        "lockd"
-        "sunrpc"
     ];
 
     # Enable and configure iSCSI service
@@ -37,28 +31,40 @@
         logDriver = "json-file";
     };
 
-    # Add NFS kernel module support with explicit version support
+    # Add NFS kernel module support
     boot.supportedFilesystems = [ "nfs" "nfs4" ];
-    boot.kernel.sysctl = {
-        "fs.nfs.nlm_timeout" = 10;
-        "fs.nfs.nlm_udpport" = 32768;
-        "fs.nfs.nlm_tcpport" = 32768;
-    };
 
-    # Install NFS utilities and additional required packages
-    environment.systemPackages = with pkgs; [
-        nfs-utils
-        nfs-utils.out
-        kmod # For modprobe
-    ];
+    # Enable rpcbind which is required for NFS
+    services.rpcbind.enable = true;
 
-    # Enable NFS server and client services
+    # Configure NFS server
     services.nfs.server = {
         enable = true;
-        exports = "";  # Configure if needed
+        # Fixed ports for firewall configuration
+        lockdPort = 4001;
+        mountdPort = 4002;
+        statdPort = 4000;
+        # Export configuration
+        exports = ''
+          /storage         *(rw,fsid=0,no_subtree_check,no_root_squash)
+          /var/lib/longhorn *(rw,no_root_squash,no_subtree_check,insecure)
+        '';
     };
-    services.nfs.client = {
-        enable = true;
-        nfsv4 = true;
+
+    # Open required firewall ports for NFSv3 and NFSv4
+    networking.firewall = {
+        allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+        allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
     };
+
+    # Install NFS utilities
+    environment.systemPackages = with pkgs; [
+        nfs-utils
+    ];
+
+    # Ensure storage directory exists and has correct permissions
+    systemd.tmpfiles.rules = [
+        "d /storage 0755 root root -"
+        "d /var/lib/longhorn 0755 root root -"
+    ];
 }
