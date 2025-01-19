d9e6b8c0b80a617c8a1bb9ccf281cfda6937e505
Yuri <yurifl03@gmail.com>
2025-01-19 17:00:27 -0300

    wip :zap:

--- nixos/common.nix
+++ nixos/common.nix
@@ -93,6 +93,20 @@ in
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    settings = {
+      # Enable binary caches
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      # Enable remote building
+      builders-use-substitutes = true;
+      trusted-users = [ "root" "nixos" ];
+    };
     nixPath = [
       "nixpkgs=https://nixos.org/channels/nixpkgs-unstable"
       "nixos-config=/etc/nixos/configuration.nix"
