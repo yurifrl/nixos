0bab7817492318f641aed78345d56e9aec6ec8f9
Yuri <yurifl03@gmail.com>
2025-01-27 18:18:26 -0300

    wip :zap:

--- nixos/machines/rpi/inotify.nix
+++ nixos/machines/rpi/inotify.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }:
 {
   # Increase system-wide file descriptor limits
-  security.pam.loginLimits = [
-    {
-      domain = "*";
-      type = "soft";
-      item = "nofile";
-      value = "16384";
-    }
-    {
-      domain = "*";
-      type = "hard";
-      item = "nofile";
-      value = "524288";
-    }
-  ];
+#   security.pam.loginLimits = [
+#     {
+#       domain = "*";
+#       type = "soft";
+#       item = "nofile";
+#       value = "16384";
+#     }
+#     {
+#       domain = "*";
+#       type = "hard";
+#       item = "nofile";
+#       value = "524288";
+#     }
+#   ];
 }
