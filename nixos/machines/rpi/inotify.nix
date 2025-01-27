40e9f8f31ad2a5e462ca5ac9988811427177ad31
Yuri <yurifl03@gmail.com>
2025-01-27 18:08:53 -0300

    wip :zap:

--- nixos/machines/rpi/inotify.nix
+++ nixos/machines/rpi/inotify.nix
@@ -1,26 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # System limits configuration
+  # Increase system-wide file descriptor limits
   security.pam.loginLimits = [
     {
       domain = "*";
       type = "soft";
       item = "nofile";
-      value = "65535";
+      value = "16384";
     }
     {
       domain = "*";
       type = "hard";
       item = "nofile";
-      value = "65535";
+      value = "524288";
     }
   ];
 
-  # Increase system-wide file descriptor limit
+  # Increase the system-wide file watch limit
   boot.kernel.sysctl = {
-    "fs.file-max" = 65535;
-    "fs.inotify.max_user_watches" = 65535;
-    "fs.inotify.max_user_instances" = 8192;
+    "fs.file-max" = 2097152;
+    "fs.inotify.max_user_watches" = 524288;
+    "fs.inotify.max_user_instances" = 512;
   };
-
 }
