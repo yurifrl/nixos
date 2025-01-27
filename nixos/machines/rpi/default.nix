935e21908e66a7388ba4d4e3e3a35ccd5e2ae601
Yuri <yurifl03@gmail.com>
2025-01-27 16:41:00 -0300

    wip :zap:

--- nixos/machines/rpi/default.nix
+++ nixos/machines/rpi/default.nix
@@ -22,7 +22,7 @@
   programs.fish.shellAliases = {
     argo-restart = "cd /home/nixos/home-systems/nixos && git pull origin main & sudo systemctl restart argo-setup & sudo journalctl -u argo-setup.service -f";
     xablaunixos = "cd /home/nixos/home-systems/nixos && git pull origin main && sudo nixos-rebuild switch --flake .#rpi --impure --show-trace";
-    xablaunixosupgrade = "cd /home/nixos/home-systems/nixos && git pull origin main && sudo nixos-rebuild switch --flake .#rpi --impure --show-trace --upgrade";
+    xablaunixosupgrade = "cd /home/nixos/home-systems/nixos && git pull origin main && sudo nix-channel --update && sudo nixos-rebuild switch --flake .#rpi --impure --show-trace --upgrade";
   };
 
   networking = {
