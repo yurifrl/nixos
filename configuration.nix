ed0b09fe839f66f1e09580956f61207aef7bba11
Yuri <yurifl03@gmail.com>
2025-03-08 22:25:10 -0300

    wip :zap:

--- configuration.nix
+++ configuration.nix
@@ -17,24 +17,4 @@
   environment.systemPackages = [
     inputs.self.packages.${pkgs.system}.cowsay-version
   ];
-
-  # Swap configuration
-  systemd.tmpfiles.rules = [
-    "d /swap 0755 root root -"
-  ];
-  
-  swapDevices = [{
-    device = "/swap/swapfile";
-    size = 1024 * 2; # 2 GB
-  }];
-
-  # Store nixpkgs in /etc/nixpkgs
-  environment.etc.nixpkgs.source = nixpkgs;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken.
-  system.stateVersion = "25.05";
-
-  # boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 } 
--- hardware.nix
+++ hardware.nix
@@ -7,10 +7,23 @@
     "${modulesPath}/virtualisation/digital-ocean-image.nix"
   ];
 
-  # Add any DigitalOcean-specific hardware configurations here
-  boot.loader.grub.device = "/dev/vda";
-  fileSystems."/" = lib.mkForce {
-    device = "/dev/vda1";
-    fsType = "ext4";
-  };
+  # Swap configuration
+  systemd.tmpfiles.rules = [
+    "d /swap 0755 root root -"
+  ];
+  
+  swapDevices = [{
+    device = "/swap/swapfile";
+    size = 1024 * 2; # 2 GB
+  }];
+
+  # Store nixpkgs in /etc/nixpkgs
+  environment.etc.nixpkgs.source = nixpkgs;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken.
+  system.stateVersion = "25.05";
+
+  # boot.binfmt.emulatedSystems = [ "x86_64-linux" ];
 } 
