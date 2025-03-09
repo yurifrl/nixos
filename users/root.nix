f66e62ca89e5740e7891b4af438c0fbc58267fcf
Yuri <yurifl03@gmail.com>
2025-03-09 00:37:52 -0300

    wip :zap:

--- users/root.nix
+++ users/root.nix
@@ -4,7 +4,7 @@
 {
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvaTuBhwuQHdjIP1k9YQk9YMqmGiOate19iXe6T4IL/", # SSH public key for root access
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvaTuBhwuQHdjIP1k9YQk9YMqmGiOate19iXe6T4IL/" # SSH public key for root access
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwxFzyCZR+V3/MKTAoxNtgPm5OLgjs8h4EpId6h54yu github-actions-deploy"
     ];
 
