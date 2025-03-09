b215d077c6db26df29582896e4202af1e87bb4d3
Yuri <yurifl03@gmail.com>
2025-03-09 01:45:57 -0300

    wip :zap:

--- nixos.sh
+++ nixos.sh
@@ -3,19 +3,19 @@
 BUILDS=(".#nixosConfigurations.digitalOcean.config.system.build.digitalOceanImage")
 CACHE="/nix-cache"
 KEY_FILE="/root/.ssh/cache-priv-key.pem"
-# Note: The key is specified without the "cache-key-1:" prefix in the trusted-public-keys option
+# Note: The key should be used directly without any prefix
 TRUSTED_KEY="$(cat /root/.ssh/cache-pub-key.pem)"
 
 
-echo "${BUILDS[@]}" | xargs nix build --system x86_64-linux
+# echo "${BUILDS[@]}" | xargs nix build --system x86_64-linux 
 
-# nix build --option substituters "${https://github.com/nix-community/cache-nix-action}" \
+# nix build "${https://github.com/nix-community/cache-nix-action}" \
 #    --option trusted-public-keys "cache-key-1:${TRUSTED_KEY}" "${BUILDS[@]}" --system x86_64-linux
 
 # nix build .#nixosConfigurations.digitalOcean.config.system.build.digitalOceanImage \
 #     --system x86_64-linux \
-#     --option substituters \
-#     --option trusted-public-keys "cache-key-1:${TRUSTED_KEY}"
+#     --store /nix-store \
+#     --option trusted-public-keys "${TRUSTED_KEY}"
 
 
 mapfile -t DERIVATIONS < <(echo "${BUILDS[@]}" | xargs nix path-info --derivation)
@@ -24,5 +24,13 @@ mapfile -t DEPENDENCIES < <(echo "${DERIVATIONS[@]}" | xargs nix-store --query -
 
 echo "${DEPENDENCIES[@]}" | xargs nix store sign --key-file "${KEY_FILE}" --recursive
 
-# Note: The "cache-key-1:" prefix is added here in the trusted-public-keys option
-echo "${DEPENDENCIES[@]}" | xargs nix copy --to "${CACHE}" --option trusted-public-keys "cache-key-1:${TRUSTED_KEY}"
+echo "${DEPENDENCIES[@]}" | xargs nix copy --to "${CACHE}"
+
+
+
+
+# $ echo "${BUILDS[@]}" | xargs nix build
+# $ mapfile -t DERIVATIONS < <(echo "${BUILDS[@]}" | xargs nix path-info --derivation)
+# $ mapfile -t DEPENDENCIES < <(echo "${DERIVATIONS[@]}" | xargs nix-store --query --requisites --include-outputs)
+# $ echo "${DEPENDENCIES[@]}" | xargs nix store sign --key-file "${KEY_FILE}" --recursive
+# $ echo "${DEPENDENCIES[@]}" | xargs nix copy --to "${CACHE}"
