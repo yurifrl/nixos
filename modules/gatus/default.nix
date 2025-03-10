3d4b77c4a5f141659ed2d893d8a01e4a7a463958
Yuri <yurifl03@gmail.com>
2025-03-10 00:23:04 -0300

    wip :zap:

--- modules/gatus/default.nix
+++ modules/gatus/default.nix
@@ -18,14 +18,32 @@
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     
+    # Prevent service from stopping during deployment
+    stopIfChanged = false;
+    
+    # Don't restart during deployment
+    restartIfChanged = false;
+    
     serviceConfig = {
       User = "gatus";
       Group = "gatus";
       ExecStart = "${pkgs.gatus}/bin/gatus";
-      # Restart = "on-failure";
+      Restart = "on-failure";
+      # More aggressive restart settings
+      RestartSec = "10s";
+      # No limit on restart attempts
+      StartLimitBurst = "0";
       # Required environment variables
       # CF_ACCESS_CLIENT_ID, CF_ACCESS_CLIENT_SECRET, DISCORD_WEBHOOK_URL
       EnvironmentFile = "/etc/gatus/gatus.env";
+    };
+    
+    # Configure unit properties to prevent failed service from affecting deployment
+    unitConfig = {
+      # Do nothing when service fails to start repeatedly
+      StartLimitAction = "none";
+      # Do nothing when service fails
+      FailureAction = "none";
     };
     
     environment = {
