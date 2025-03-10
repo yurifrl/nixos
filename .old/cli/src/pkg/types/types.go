7262c7e2dbbbe912bfa916074735e2bfcc8d756c
Yuri <yurifl03@gmail.com>
2025-03-10 00:47:02 -0300

    wip :zap:

--- .old/cli/src/pkg/types/types.go
+++
@@ -1,8 +0,0 @@
-package types
-
-import "bytes"
-
-// Executor defines the interface for executing commands
-type Executor interface {
-	ExecuteCommand(name string, arg ...string) (bytes.Buffer, error)
-}
