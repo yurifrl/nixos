7b748df7441dcfcffce53602fe75a81c7dbb79e8
Yuri <yurifl03@gmail.com>
2025-03-15 01:06:34 -0300

    wip :zap:

--- users/root.nix
+++ users/root.nix
@@ -4,8 +4,8 @@
 {
   users.users.root = {
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvaTuBhwuQHdjIP1k9YQk9YMqmGiOate19iXe6T4IL/" # SSH public key for root access
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICPvhdB5G3x/vLkM3wGQC+Ug0xHFCAVqAwCmNqRTFnM8 github-actions-deploy"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAvaTuBhwuQHdjIP1k9YQk9YMqmGiOate19iXe6T4IL/" # Local -> 45.55.248.197
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICPvhdB5G3x/vLkM3wGQC+Ug0xHFCAVqAwCmNqRTFnM8 github-actions-deploy" # Github -> 45.55.248.197
     ];
 
     # Hashed password for root console access
